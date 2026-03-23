f3488cd4b4d208db0e54c90e356bbb89e3dea2f0
Ritik Kumar Gupta <ririkkumargupta@gmail.com>
2026-03-23 19:12:25 +0530

    First commit

---
+++ Loop control/ques_break_keyword12.java
@@ -0,0 +1,34 @@
+public class ques_break_keyword12 {
+     
+            static int add(int a,int b){
+                return a+b;
+            }
+        // without return type
+    public static void main(String[] args) {
+        
+        /*
+         * int n = 4;
+         * int sum = 0;
+         * int i = 1;
+         * 
+         * while (i <= n) {
+         * sum = sum + i;
+         * i++;
+         * 
+         * }
+         * System.out.println(sum);
+         */
+       /*  int n = 10072006;
+        while (n>0) {
+            int lastdigit = n % 10;
+            System.out.print(lastdigit);
+            n = n/10;
+            
+        } 
+        System.out.println();*/
+       int result = add(2,4);
+       System.out.print(result);
+
+       
+    }
+}
--- stringh.java
+++ stringh.java
@@ -1,9 +1,54 @@
+import java.util.Scanner;
+
 public class stringh {
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         String s1 = "HELLO";
         String s2 = "hello";
-        System.out.println(s1== s2);
-    }
-    
-}
+        System.out.println(s1 == s2);
 
+        {
+            int n = 11;
+            if (n % 2 == 0) {
+                System.out.println("even");
+
+            } else {
+                System.out.println("odd");
+            }
+
+        }
+        {
+            // by user chek even or not
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter your value : ");
+            int n = sc.nextInt();
+            if (n % 2 == 0) {
+                System.out.println("Even");
+
+            } else {
+                System.out.println("odd");
+            }
+
+        }
+        {
+            int count = 1;
+            do {
+                System.out.print(count);
+                count++;
+            } while (count <= 10);
+        }
+
+        {
+            // int n;
+            for (int i = 1; i <= 10; i++) {
+                if (i == 4) {
+                    break;
+
+                }
+            System.out.println(" value count is : " + i);
+
+            }
+         System.out.println("I am not a loop.....");
+        }
+    }
+
+}
