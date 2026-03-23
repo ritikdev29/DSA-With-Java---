04549864b18e51d41be712bbc19826ef6f64830e
Ritik Kumar Gupta <ririkkumargupta@gmail.com>
2026-03-23 22:57:11 +0530

    inverted_rotated_half_pyramid

--- Pattern Part_2 Advance/Pattern.java
+++ Pattern Part_2 Advance/Pattern.java
@@ -17,11 +17,25 @@ public class Pattern {
         System.out.println();
 
         }
-        // System.out.println();
+    
+    }
+    public static void inverted_rotated_half_pyramid(int n){
+        for(int i=1;i<=n;i++){
+            // spaces
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            // star
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
         hollow_rectangle(4, 5);
+        inverted_rotated_half_pyramid(4);
     }
 
 }
