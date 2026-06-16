468f3a585d4101458ec0f0b41cb0943148cd0dbe
Ritik Kumar Gupta <ririkkumargupta@gmail.com>
2026-06-17 04:47:47 +0530

    Binary XOR Operations

---
+++ 17.Bit manipulation/binary_XOR_06.java
@@ -0,0 +1,23 @@
+public class binary_XOR_06 {
+    
+    public static void main(String[] args) {
+        int a = 5;
+        int  b= 6;
+
+        // or operator
+        int result = a^b;
+        System.out.println("A binary 5 : " + Integer.toBinaryString(a));
+        System.out.println("B binary 6 : " + Integer.toBinaryString(b));
+
+        System.out.println("Result Binary  : " + Integer.toBinaryString(result));
+        System.out.println("Result Decimal  : " +result);
+
+
+
+        System.out.println( "Your OR Operator values is : "+(5^ 6));
+    }
+    
+}
+
+    
+
