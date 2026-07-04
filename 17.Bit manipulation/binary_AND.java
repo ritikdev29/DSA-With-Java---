b85212b7dfed6b7637aabe142525c3f82845c06f
Ritik Kumar Gupta <ririkkumargupta@gmail.com>
2026-07-04 17:53:57 +0530

    ....

--- 17.Bit manipulation/binary_AND.java
+++ 17.Bit manipulation/binary_AND.java
@@ -1,22 +1,28 @@
 public class binary_AND {
     public static void main(String[] args) {
-        int user1= 13;  // binary no is = 1101
+        int user1 = 13; // binary no is = 1101
         int user2 = 11; // binary no is = 1011
 
         // AND operator dononumber par make
 
-       int commonPermission =  user1 & user2;
+        int commonPermission = user1 & user2;
 
-       // Integer.toBinaryString() decimal to binary conver karta hai...
+        // Integer.toBinaryString() decimal to binary conver karta hai...
 
-       System.out.println("User1 binary number : " + Integer.toBinaryString(user1));
-       System.out.println("user2 Binary :" + Integer.toBinaryString(user2));
+        System.out.println("User1 binary number : " + Integer.toBinaryString(user1));
+        System.out.println("user2 Binary :" + Integer.toBinaryString(user2));
 
-       System.out.println("Common permission Binary : " + Integer.toBinaryString(commonPermission));
+        System.out.println("Common permission Binary : " + Integer.toBinaryString(commonPermission));
 
+        System.out.println("Common permission decimal : " + commonPermission); // outpt = 9
 
-       System.out.println("Common permission decimal : " + commonPermission); // outpt = 9
+        /* output =
+         * User1 binary number : 1101
+         * user2 Binary :1011
+         * Common permission Binary : 1001
+         * Common permission decimal : 9
+         */
 
     }
-    
+
 }
--- 17.Bit manipulation/binary_OR05.java
+++ 17.Bit manipulation/binary_OR05.java
@@ -1,19 +1,17 @@
 public class binary_OR05 {
     public static void main(String[] args) {
         int a = 5;
-        int  b= 6;
+        int b = 6;
 
         // or operator
-        int result = a|b;
+        int result = a | b;
         System.out.println("A binary  : " + Integer.toBinaryString(a));
         System.out.println("B binary  : " + Integer.toBinaryString(b));
 
         System.out.println("Result Binary  : " + Integer.toBinaryString(result));
-        System.out.println("Result Decimal  : " +result);
+        System.out.println("Result Decimal  : " + result);
 
-
-
-        System.out.println( "Your OR Operator values is : "+(5| 6));
+        System.out.println("Your OR Operator values is : " + (5 | 6));
     }
-    
+
 }
